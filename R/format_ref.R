07343035988bb0a5ce29c544ec349478374f63eb
WANG Zhiwei <48282751+statwangz@users.noreply.github.com>
2022-01-19 03:00:55 +0800

    Update format_ref.R

--- R/format_ref.R
+++ R/format_ref.R
@@ -18,7 +18,7 @@ format_ref <- function(file_ref, sumstats) {
   # merge the reference data
   ref_X <- bind_cols(SNP = rownames(ref$X), as_tibble(ref$X)) %>%
     drop_na() %>%
-    semi_join(sumstats, sumstats, by = "SNP")
+    semi_join(sumstats, by = "SNP")
   message("Remove missing data and merge the reference data with summary statistics data..., remaining ", nrow(ref_X), " SNPs.")
 
   # merge the summary statistics data with reference data
