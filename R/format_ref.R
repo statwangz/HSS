0131277bf48a5049952d784b3f4c433d7648866c
WANG Zhiwei <48282751+statwangz@users.noreply.github.com>
2022-01-21 02:24:47 +0800

    update

--- R/format_ref.R
+++ R/format_ref.R
@@ -1,37 +1,37 @@
 #' Format the reference data.
 #'
-#' @param file_ref name of the reference data file
+#' @param X_ref reference genotypes data
 #' @param sumstats summary statistics data
 #' @param group LD block group
 #'
 #' @return z-score and reference data
 #' @export
 #'
-format_ref <- function(file_ref, sumstats, group = NULL) {
+format_ref <- function(X_ref, sumstats, group = NULL) {
   message("Begin reading in reference data...")
   ref <- genio::read_plink(file_ref)
-  message("The reference data set has ", nrow(ref$X), " lines.")
+  message("The reference data set has ", nrow(X_ref), " lines.")
 
   if (!is.null(group)) {
     sumstats <- inner_join(sumstats, group, by = "SNP")
   }
 
   # merge the reference data
-  ref_X <- bind_cols(SNP = rownames(ref$X), as_tibble(ref$X)) %>%
+  X_ref <- bind_cols(SNP = rownames(X_ref), as_tibble(X_ref)) %>%
     drop_na() %>%
     semi_join(sumstats, by = "SNP")
-  message("Remove missing data and merge the reference data with summary statistics data..., remaining ", nrow(ref_X), " SNPs.")
+  message("Remove missing data and merge the reference data with summary statistics data..., remaining ", nrow(X_ref), " SNPs.")
 
   # merge the summary statistics data with reference data
-  sumstats <- semi_join(sumstats, ref_X, by = "SNP")
+  sumstats <- semi_join(sumstats, X_ref, by = "SNP")
   message("Merge the summary statistics data with reference data..., remaining ", nrow(sumstats), " SNPs.")
 
   if (!is.null(group)) {
     z <- select(sumstats, Z, N, GROUP)
-    X <- t(as.matrix(select(ref_X, -SNP)))
+    X <- t(as.matrix(select(X_ref, -SNP)))
   } else {
     z <- select(sumstats, Z, N)
-    X <- t(as.matrix(select(ref_X, -SNP)))
+    X <- t(as.matrix(select(X_ref, -SNP)))
   }
 
   return(xpass_data = list(z = z, X = X))
